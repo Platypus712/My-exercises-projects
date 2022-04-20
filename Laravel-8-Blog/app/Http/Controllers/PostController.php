f0451966f949a804e602bb25eb2278cdede8bb65
Mr. Psyduck <85735602+Platypus712@users.noreply.github.com>
2022-04-20 19:51:59 +0200

    Add files via upload

---
+++ Laravel-8-Blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    public function index() 
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
+        ]);
+    }
+
+    public function show(Post $post) 
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
+}
+
